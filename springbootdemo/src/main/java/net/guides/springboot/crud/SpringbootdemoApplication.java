0f8f56f3fab4f5ca877e18f6a7f6a1315c5ed328
Jumanahalhazba <97724418+Jumanahalhazba@users.noreply.github.com>
2022-01-28 10:24:44 +0200

    Structure and repository

--- springbootdemo/src/main/java/net/guides/springboot/crud/SpringbootdemoApplication.java
+++ springbootdemo/src/main/java/net/guides/springboot/crud/SpringbootdemoApplication.java
@@ -1,13 +1,12 @@
-package com.example.springbootdemo;
+package net.guides.springboot.crud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootdemoApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootdemoApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
-
 }

ccdaa5e519125030c079fc1fe9db05b0a458ecef
Jumanahalhazba <97724418+Jumanahalhazba@users.noreply.github.com>
2022-02-12 22:41:33 +0200

    Added serviceWorker and router dom fix

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import ListEmployeeComponent from './components/ListEmployeeComponent';
 import HeaderComponent from './components/HeaderComponent';
 import FooterComponent from './components/FooterComponent';
@@ -14,13 +14,13 @@ function App() {
         <Router>
               <HeaderComponent />
                 <div className="container">
-                    <Switch>
+                    <Routes>
                           <Route path = "/" exact component = {ListEmployeeComponent}></Route>
                           <Route path = "/employees" component = {ListEmployeeComponent}></Route>
                           <Route path = "/add-employee/:id" component = {CreateEmployeeComponent}></Route>
                           <Route path = "/view-employee/:id" component = {ViewEmployeeComponent}></Route>
                           {/* <Route path = "/update-employee/:id" component = {UpdateEmployeeComponent}></Route> */}
-                    </Switch>
+                    </Routes>
                 </div>
               <FooterComponent />
         </Router>
--- react-frontend/src/components/FooterComponent.jsx
+++ react-frontend/src/components/FooterComponent.jsx
@@ -12,7 +12,7 @@ class FooterComponent extends Component {
         return (
             <div>
                 <footer className = "footer">
-                    <span className="text-muted">All Rights Reserved 2020 @JavaGuides</span>
+                    <span className="text-muted">Personal project test</span>
                 </footer>
             </div>
         )

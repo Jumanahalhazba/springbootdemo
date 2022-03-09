6e346692e5b6970f7afc073e3a2bd9e68f59dd1e
Jumanahalhazba <97724418+Jumanahalhazba@users.noreply.github.com>
2022-03-09 10:46:41 +0200

    Added dependency,model and mapping for image upload

--- springbootdemo/src/main/java/net/guides/springboot/crud/controller/EmployeeController.java
+++ springbootdemo/src/main/java/net/guides/springboot/crud/controller/EmployeeController.java
@@ -1,27 +1,34 @@
 package net.guides.springboot.crud.controller;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+//added for image
+import java.io.File;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import net.guides.springboot.crud.exception.ResourceNotFoundException;
 import net.guides.springboot.crud.model.Employee;
 import net.guides.springboot.crud.repository.EmployeeRepository;
 import net.guides.springboot.crud.service.SequenceGeneratorService;
+import org.apache.commons.io.FilenameUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -77,5 +84,29 @@ public class EmployeeController {
         Map < String, Boolean > response = new HashMap < > ();
         response.put("deleted", Boolean.TRUE);
         return response;
+    }
+    //image add
+    private static String imageDirectory = System.getProperty("user.dir") + "/images/";
+    //@GetMapping(value = "/image", produces = {MediaType.IMAGE_PNG_VALUE, "application/json"})
+    @GetMapping("/image")
+    public String test(){return "Success";}
+    public ResponseEntity<?> uploadImage(@RequestParam("imageFile")MultipartFile file,
+                                         @RequestParam("imageName") String name) {
+        makeDirectoryIfNotExist(imageDirectory);
+        Path fileNamePath = Paths.get(imageDirectory,
+                name.concat(".").concat(FilenameUtils.getExtension(file.getOriginalFilename())));
+        try {
+            Files.write(fileNamePath, file.getBytes());
+            return new ResponseEntity<>(name, HttpStatus.CREATED);
+        } catch (IOException ex) {
+            return new ResponseEntity<>("Image is not uploaded", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    private void makeDirectoryIfNotExist(String imageDirectory) {
+        File directory = new File(imageDirectory);
+        if (!directory.exists()) {
+            directory.mkdir();
+        }
     }
 }
